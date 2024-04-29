d240fda233100d74ede6e21451b476dfb635ba61
Clo91eaf <Clo91eaf@qq.com>
2024-04-29 13:49:01 +0800

    add submodule verilator to support difftest

--- build.rs
+++ build.rs
@@ -1,44 +1,44 @@
-// use std::path::Path;
-// use std::path::PathBuf;
-// use std::{env, fs};
-// use verilator::gen::{Standard, Verilator};
-// use verilator::module::ModuleGenerator;
+use std::path::Path;
+use std::path::PathBuf;
+use std::{env, fs};
+use verilator::gen::{Standard, Verilator};
+use verilator::module::ModuleGenerator;
 
-// macro_rules! t {
-//   ($e:expr) => {
-//     match $e {
-//       Ok(e) => e,
-//       Err(e) => panic!("{} failed with {}", stringify!($e), e),
-//     }
-//   };
-// }
+macro_rules! t {
+  ($e:expr) => {
+    match $e {
+      Ok(e) => e,
+      Err(e) => panic!("{} failed with {}", stringify!($e), e),
+    }
+  };
+}
 
 fn main() {
-//   let out_dir = env::var("OUT_DIR").unwrap();
-//   let out_dir = PathBuf::from(out_dir);
-//   let _ = fs::remove_dir_all(&out_dir);
-//   t!(fs::create_dir_all(&out_dir));
+  let out_dir = env::var("OUT_DIR").unwrap();
+  let out_dir = PathBuf::from(out_dir);
+  let _ = fs::remove_dir_all(&out_dir);
+  t!(fs::create_dir_all(&out_dir));
 
-//   // Generate CPP shim from RUST
-//   let mut module = ModuleGenerator::default();
-//   module.generate("src/dut/top.rs");
+  // Generate CPP shim from RUST
+  let mut module = ModuleGenerator::default();
+  module.generate("src/dut/top.rs");
 
-//   // Generate CPP from Verilog
-//   let mut verilator = Verilator::default();
+  // Generate CPP from Verilog
+  let mut verilator = Verilator::default();
 
-//   let dir = Path::new("myCPU");
-//   let files: Vec<_> = fs::read_dir(dir)
-//     .expect("Directory not found")
-//     .filter_map(|entry| entry.ok().and_then(|e| e.path().to_str().map(|s| String::from(s))))
-//     .collect();
+  let dir = Path::new("myCPU");
+  let files: Vec<_> = fs::read_dir(dir)
+    .expect("Directory not found")
+    .filter_map(|entry| entry.ok().and_then(|e| e.path().to_str().map(|s| String::from(s))))
+    .collect();
 
-//   println!("Files: {:?}", files);
+  println!("Files: {:?}", files);
 
-//   verilator.with_coverage(true).with_trace(true);
+  verilator.with_coverage(true).with_trace(true);
 
-//   for file in &files {
-//     verilator.file_with_standard(file, Standard::Verilog2005);
-//   }
+  for file in &files {
+    verilator.file_with_standard(file, Standard::Verilog2005);
+  }
 
-//   verilator.file(out_dir.join("mycpu_top.cpp")).build("mycpu_top");
+  verilator.file(out_dir.join("mycpu_top.cpp")).build("mycpu_top");
 }
--- src/dut.rs
+++ src/dut.rs
@@ -1,21 +1,21 @@
-// mod top;
+mod top;
 
-// // sram interface
-// struct Dut {
-//   // trace debug interface
-//   pub debug_wb_pc: [bool; 32],
-//   pub debug_wb_rf_wen: [bool; 4],
-//   pub debug_wb_rf_wnum: [bool; 5],
-//   pub debug_wb_rf_wdata: [bool; 32],
-// }
+// sram interface
+struct Dut {
+  // trace debug interface
+  pub debug_wb_pc: [bool; 32],
+  pub debug_wb_rf_wen: [bool; 4],
+  pub debug_wb_rf_wnum: [bool; 5],
+  pub debug_wb_rf_wdata: [bool; 32],
+}
 
-// impl Dut {
-//   fn new(rtl_files: Vec<String>) -> Self {
-//     Self {
-//       debug_wb_pc: [false; 32],
-//       debug_wb_rf_wen: [false; 4],
-//       debug_wb_rf_wnum: [false; 5],
-//       debug_wb_rf_wdata: [false; 32],
-//     }
-//   }
-// }
+impl Dut {
+  fn new(rtl_files: Vec<String>) -> Self {
+    Self {
+      debug_wb_pc: [false; 32],
+      debug_wb_rf_wen: [false; 4],
+      debug_wb_rf_wnum: [false; 5],
+      debug_wb_rf_wdata: [false; 32],
+    }
+  }
+}
